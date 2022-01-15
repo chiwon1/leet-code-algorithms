c5f3da4b85e03e68d4b51549c833f8f3e1d15432
Chiwon Choi <emphasize1@gmail.com>
2022-01-15 20:12:37 +0900

    Time: 94 ms (30.79%), Space: 39.7 MB (55.76%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            return i;
+        } else if (nums[i] > target) {
+            return i;
+        } else if (i === nums.length - 1) {
+            return nums.length;
+        }
+    }
+};
+
+
+// 있는 경우
+// - index 리턴
+// 없는 경우
+// 1. 중간에 들어가야 하는 경우
+// 2. 마지막에 들어가야 하는 경우
