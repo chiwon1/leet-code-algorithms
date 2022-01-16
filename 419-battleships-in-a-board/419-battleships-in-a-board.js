927dddb437c97e8b49c16a4a5400f0a9a6c6ff56
Chiwon Choi <emphasize1@gmail.com>
2022-01-16 15:03:54 +0900

    Time: 129 ms (9.76%), Space: 40 MB (86.18%) - LeetHub

---
+++ 419-battleships-in-a-board/419-battleships-in-a-board.js
@@ -0,0 +1,37 @@
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+var countBattleships = function(board) {
+    function dfs(x, y) {
+        if (x <= -1 || x >= board.length || y <= -1 || y >= board[0].length) {
+            return false;
+        }
+
+        if (board[x][y] === 'X') {
+            board[x][y] = '.';
+
+            dfs(x - 1, y);
+            dfs(x, y - 1);
+            dfs(x + 1, y);
+            dfs(x, y + 1);
+
+            return true;
+        }
+
+        return false;
+    }
+    
+    let count = 0;
+    
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (dfs(i, j)) {
+                count++;
+            }
+        }
+    }
+    
+    return count;
+};
+
