84992bffbf2e527ffa219109d9937352f436630c
Chiwon Choi <emphasize1@gmail.com>
2022-02-20 14:53:33 +0900

    Time: 146 ms (42.51%), Space: 49.2 MB (33.31%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function(n) {
+    const result = [];
+    
+    for (let i = 0; i < n + 1; i++) {
+        const binaryValue = i.toString(2).toString();
+        let count = 0;
+        
+        for (let j = 0; j < binaryValue.length; j++) {
+            if (Number(binaryValue[j]) === 1) {
+                count++;
+            }
+        }
+        
+        result.push(count);
+    }
+    
+    return result;
+};
